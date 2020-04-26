37743a183026c29cffd3c7f12642451ee13aa133
Michael Pedersen Starmer <michael.starmer@gmail.com>
2020-04-26 20:46:31 +0200

    UPDATE modules.

--- src/modules/src/main.rs
+++ src/modules/src/main.rs
@@ -4,8 +4,26 @@
  * To enable all logging: RUST_LOG=trace cargo run
  */
 
-use rand::prelude::*;
 mod logger;
+mod cmod {
+
+   // private
+   fn showData() {
+      info!("Log from private function.")
+   }
+
+   // public
+   pub fn print_message() {
+      info!("Module initialized");
+      showData();
+   }
+
+   pub mod helper {
+      pub fn print_message() {
+         info!("Helper function")
+      }
+   }
+}
 
 #[macro_use] extern crate log;
 
@@ -13,5 +31,7 @@ mod logger;
 fn main() {
    
    logger::init_application();
+   cmod::print_message();
+   cmod::helper::print_message();
    
 }
