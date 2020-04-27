d7f0189649818d0e9c9df73a22e32e78868fc243
Michael Pedersen Starmer <michael.starmer@gmail.com>
2020-04-27 20:29:52 +0200

    REFACTOR logger and ADD new functions

--- _template/src/logger.rs
+++ _template/src/logger.rs
@@ -1,9 +1,33 @@
 extern crate pretty_env_logger;
 
-/// Initialize logger and display some examples.
-pub fn init_application() {
-  pretty_env_logger::init();
+use std::process::Command;
 
+fn main()
+{
+  pretty_env_logger::init();
+}
+
+/// Initialize logger and display some examples.
+pub fn init_application()
+{
+  pretty_env_logger::init();
+}
+
+/// Clears the terminal window.
+#[allow(dead_code)]
+pub fn clear_terminal()
+{
+  let output = Command::new("clear").output().unwrap_or_else(|e| {
+    panic!("failed to execute process: {}", e)
+  });
+
+  println!("{}", String::from_utf8_lossy(&output.stdout));
+}
+
+/// Display all available log types and the function name.
+#[allow(dead_code)]
+pub fn print_log_types()
+{
   let mut header = String::new();
   let title = String::from("Application is running.");
 
@@ -20,5 +44,4 @@ pub fn init_application() {
   error!("error!(\"message\")");
   println!("\n{}", &title);
   println!("\n{}==", &header);
-   
 }
